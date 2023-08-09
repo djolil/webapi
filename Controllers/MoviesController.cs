ae41005b5ea88159fd504b47563b90aa5ef234e6
Абдулджалил Рагимов <ragimov.07112002@inbox.ru>
2023-08-09 12:15:13 +0300

    DTO

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using webapi.Models;
+using webapi.Models.DTO;
 
 namespace webapi.Controllers
 {
@@ -22,18 +23,18 @@ namespace webapi.Controllers
 
         // GET: api/Movies
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
+        public async Task<ActionResult<IEnumerable<MovieDTO>>> GetMovies()
         {
             if (_context.Movies == null)
             {
                 return NotFound();
             }
-            return await _context.Movies.ToListAsync();
+            return await _context.Movies.Select(x => ConvertToMovieDTO(x)).ToListAsync();
         }
 
         // GET: api/Movies/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Movie>> GetMovie(int id)
+        public async Task<ActionResult<MovieDTO>> GetMovie(int id)
         {
             if (_context.Movies == null)
             {
@@ -46,17 +47,19 @@ namespace webapi.Controllers
                 return NotFound();
             }
 
-            return movie;
+            return ConvertToMovieDTO(movie);
         }
 
         // PUT: api/Movies/5
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutMovie(int id, Movie movie)
+        public async Task<IActionResult> PutMovie(int id, MovieDTO movieDTO)
         {
-            if (id != movie.Id)
+            if (id != movieDTO.Id)
             {
                 return BadRequest();
             }
+
+            Movie movie = ConvertToMovie(movieDTO);
 
             _context.Entry(movie).State = EntityState.Modified;
 
@@ -81,16 +84,19 @@ namespace webapi.Controllers
 
         // POST: api/Movies
         [HttpPost]
-        public async Task<ActionResult<Movie>> PostMovie(Movie movie)
+        public async Task<ActionResult<Movie>> PostMovie(MovieDTO movieDTO)
         {
             if (_context.Movies == null)
             {
                 return Problem("Entity set 'KinoContext.Movies'  is null.");
             }
+
+            Movie movie = ConvertToMovie(movieDTO);
+
             _context.Movies.Add(movie);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction(nameof(GetMovie), new { id = movie.Id }, movie);
+            return CreatedAtAction(nameof(GetMovie), new { id = movieDTO.Id }, movieDTO);
         }
 
         // DELETE: api/Movies/5
@@ -116,6 +122,46 @@ namespace webapi.Controllers
         private bool MovieExists(int id)
         {
             return (_context.Movies?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+        private static MovieDTO ConvertToMovieDTO(Movie movie)
+        {
+            return new MovieDTO
+            {
+                Id = movie.Id,
+                Title = movie.Title,
+                Budget = movie.Budget,
+                Overview = movie.Overview,
+                Popularity = movie.Popularity,
+                ReleaseDate = movie.ReleaseDate,
+                Revenue = movie.Revenue,
+                Runtime = movie.Runtime,
+                MovieStatus = movie.MovieStatus,
+                VoteAverage = movie.VoteAverage,
+                VoteCount = movie.VoteCount,
+                AgeRating = movie.AgeRating,
+                MediaSource = movie.MediaSource
+            };
+        }
+
+        private static Movie ConvertToMovie(MovieDTO movieDTO)
+        {
+            return new Movie
+            {
+                Id = movieDTO.Id,
+                Title = movieDTO.Title,
+                Budget = movieDTO.Budget,
+                Overview = movieDTO.Overview,
+                Popularity = movieDTO.Popularity,
+                ReleaseDate = movieDTO.ReleaseDate,
+                Revenue = movieDTO.Revenue,
+                Runtime = movieDTO.Runtime,
+                MovieStatus = movieDTO.MovieStatus,
+                VoteAverage = movieDTO.VoteAverage,
+                VoteCount = movieDTO.VoteCount,
+                AgeRating = movieDTO.AgeRating,
+                MediaSource = movieDTO.MediaSource
+            };
         }
     }
 }
---
+++ Models/DTO/MovieDTO.cs
@@ -0,0 +1,30 @@
+﻿namespace webapi.Models.DTO;
+
+public partial class MovieDTO
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public int Budget { get; set; }
+
+    public string Overview { get; set; } = null!;
+
+    public int Popularity { get; set; }
+
+    public DateOnly ReleaseDate { get; set; }
+
+    public int Revenue { get; set; }
+
+    public int Runtime { get; set; }
+
+    public string MovieStatus { get; set; } = null!;
+
+    public decimal VoteAverage { get; set; }
+
+    public int VoteCount { get; set; }
+
+    public string AgeRating { get; set; } = null!;
+
+    public string? MediaSource { get; set; }
+}
